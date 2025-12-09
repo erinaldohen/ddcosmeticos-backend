8571f18a46c05138aa7c0f3e890bc40cb8c5b06f
Erinaldo Henrique <hencomjesus@hotmail.com>
2025-12-08 21:43:55 -0300

    Criação do projeto backend.

---
+++ src/main/java/br/com/lojaddcosmeticos/ddcosmeticos_backend/DdcosmeticosBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.lojaddcosmeticos.ddcosmeticos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdcosmeticosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdcosmeticosBackendApplication.class, args);
+	}
+
+}

e94000ada9be205ad74998555e3faf270f8bfaa4
Erinaldo Henrique <hencomjesus@hotmail.com>
2026-01-29 02:02:49 -0300

    Implementação das alterações do frontend (Configurações)
    Signed-off-by: Erinaldo Henrique <hencomjesus@hotmail.com>

--- src/main/java/br/com/lojaddcosmeticos/ddcosmeticos_backend/infrastructure/WebConfig.java
+++ src/main/java/br/com/lojaddcosmeticos/ddcosmeticos_backend/infrastructure/WebConfig.java
@@ -1,4 +1,4 @@
-package br.com.lojaddcosmeticos.ddcosmeticos_backend.config;
+package br.com.lojaddcosmeticos.ddcosmeticos_backend.infrastructure;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
